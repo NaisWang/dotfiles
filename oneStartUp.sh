c8f588daef926d0d63a2675eef157d9af7cfeece
whz <1152957995@qq.com>
2023-10-10 10:03:42 +0800

    2023年10月10日 星期二 10时03分42秒 CST

---
+++ oneStartUp.sh
@@ -0,0 +1,9 @@
+# 一键启动位于副屏的所有软件
+open /Applications/WeChat.app
+open /Applications/QQ.app
+open /System/Applications/System\ Settings.app/
+open /System/Applications/Mail.app/
+open /System/Applications/Reminders.app/
+open /System/Applications/Music.app/
+open /System/Library/CoreServices/Finder.app/
+open /System/Applications/App\ Store.app/
